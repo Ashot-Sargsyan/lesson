2d060bf95109a89a518854ae1c6ce3893876efc4
Ashot-Sargsyan <77446410+Ashot-Sargsyan@users.noreply.github.com>
2021-01-14 18:15:12 +0400

    Python Conditions and If

---
+++ lesspn8.py
@@ -0,0 +1,107 @@
+# a=27
+# b=14
+# if  a>b:
+# 	print('a is big')
+
+
+
+# a=34
+# b=14
+# if  b>a:
+# 	print('b is big')
+# elif a>b:
+# 	print('a is big')
+
+
+
+# a=34
+# b=14
+# if  b>a:
+# 	print('b is big')
+# else:
+# 	print('a is big')
+
+
+# a=34
+# b=14
+# c=12
+# d=27
+# if a>b and d>c:
+# 	print('a and b are bigger')
+# else:
+# 	print('a and b are smol')
+
+
+
+# dog_year=int(input('dogs year is:'))
+# if dog_year<0:
+# 	print('error')
+# elif dog_year<3 and dog_year>0:
+#  	print('human years is:',dog_year*5.25) 
+
+# else:
+# 	res = 10.5 + (dog_year-2) * 4
+# 	print('human age is',res)
+
+
+# word1 = input('word1: ')
+# word_count = 0
+# number_count =0
+# if word1[0].isalpha():
+# 	word_count += 1
+# elif word1[0].isdigit():
+# 	number_count+=1	
+# if word1[1].isalpha():
+# 	word_count += 1
+# elif word1[1].isdigit():
+# 	number_count+=1	
+# if word1[2].isalpha():
+# 	word_count += 1
+# elif word1[2].isdigit():
+# 	number_count+=1
+
+# print(word_count,number_count)				
+
+
+
+# pasword='ash1996'
+# if len (pasword)>8:
+# 	print('pasword is strong',pasword)
+# else:
+# 	print('no')
+
+
+# x1=float(input('number one'))
+# choice= input('+ - * / ')
+# x2=float(input('number two'))
+# if choice=='+':
+# 	print('our number is equa',x1+x2)
+# elif choice=='-':
+# 	print('our number is equa',x1-x2)
+# elif choice=='*':
+# 	print('our number is equa',x1*x2)
+# elif choice=='/':
+# 	print('our number is equa',x1/x2)
+# else:
+# 	print('I  dont understand your command')
+
+
+# name='Ash Arnak Ani '
+# user1=input('')
+# if user1 in name:
+# 	print('very well',user1)
+# else:
+# 	print('sorry you wrong input')	
+
+
+# ballance=1000
+# shoping =float(input(''))
+# res=(shoping*510)
+# if shoping<0:
+# 	print('you dont pay')
+# elif res<ballance:
+# 	print('you pay to product  ',res)
+# else:
+# 	print('sorry you dont pay ',res)
+
+
