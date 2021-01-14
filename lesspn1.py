3f66b303ccfe68bb9dd25b3f0b261dfba4725027
Ashot-Sargsyan <77446410+Ashot-Sargsyan@users.noreply.github.com>
2021-01-14 18:04:31 +0400

    About python

---
+++ lesspn1.py
@@ -0,0 +1,36 @@
+# print("hello world")
+# a=5
+# b=6
+# print(a+b)
+# print("5+5")
+# print(5+5)
+# name = input('what is your name ? ') #mutkenq anum mer anune
+# print('hi my name is', name)
+# 	
+# age= int(input('please say me your age '))
+# print(age + 0)
+# age= input('please say me your age ')
+# print(age + 0)
+
+
+# born=int(input('please say me your age'))
+# print(2020-born)
+
+# number=int(input('num1 = '))
+# number2=int(input('num2 = '))
+# print(number+number2)
+# age =input('please say me youe age ' )
+# print('I am', age , 'years old')
+
+
+# print("hello company EAGL")
+# phone = input('please write your contace ')
+# name  = input('please write yourname ')
+# mail  =	input('please write your mail ')
+# print('my name is:',name,'\nphone is:', phone, '\nmail is:' ,mail)
+
+print("hello company EAGL")
+phone = input('please write your contace ')
+name  = input('please write yourname ')
+mail  =	input('please write your mail ')
+print('\t my name is:',name,'\nphone is:', phone, '\nmail is:' ,mail)
