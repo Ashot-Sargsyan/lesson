fa48f5db36aca8cae0d410bfadbf25b51be3d6a6
Ashot-Sargsyan <77446410+Ashot-Sargsyan@users.noreply.github.com>
2021-01-14 18:11:08 +0400

    Python Exercises

---
+++ lesspn4.py
@@ -0,0 +1,93 @@
+# celsius=float(input('celsius is :' ))
+# fahrenheit=(celsius *9/5)+32
+# print(fahrenheit)
+
+# fahrenheit=float(input('fahrenheit is:' ))
+# celsius=(fahrenheit-32)*5/9
+# print(celsius)
+
+# minute=float(input('minute is:' ))
+# second=minute*60
+# print(second)
+
+# day=int(input('how day is :' ))
+# minute=day=24*60
+# second=minute*60
+# print(minute,second)
+
+# a=float(input('a= '))
+# b=float(input('b= '))
+# result=a+b<=180
+# print('the amount of glos is less',result)#сторона углов а+б меньше 180
+# c=180-(a+b)
+# result_forc=c==90
+# print('angle is 90 degree',c,result_forc)
+
+# number=float(input('our number is : '))
+# result=number>=100
+# print(result)
+
+# number=float(input('our number is : '))
+# result=number!=100
+# print(result)
+
+# number=float(input('our number is : ' ))
+# result=number%400==0
+# print(result)
+
+# r=float(input('wheel is : ' ))#2pi radius okrujnosti 
+# wheel=2*22/7 *r
+# res = 5 * wheel
+# print(res)
+
+# myname='Ash'
+# name = input('what is your name?') == myname
+# print(name)
+
+# login = input('what is your name?')
+# login2 = input('what is your name?')==login
+# password1=input('creat your password1')
+# password2=input('creat your password2')==password1
+# print('your password is Valid',password2,'\nyour login is Valid',login )
+
+# question=input('do you have a asus:')=='yes'
+# i7=input('do you have a asus:')=='yes'
+# print(question,'\n',i7)
+
+
+# print('do you play a millionare')
+# many=0
+# question1 = input('how much second is 1 minute?\n a)10 b)20 c)60')=='c'
+# if question1:
+# 	many += 1000
+# 	print(many)
+# else:
+# 	print(many)
+
+
+
+# question=input('do you have a asus:')=='yes'
+# i7=input('do you have a asus:')=='yes'
+# result=question and i7
+# print(question,'\n',i7)
+
+# question=input('do you have a asus:')=='yes'
+# i7=input('do you have a asus:')=='yes'
+# result=question or i7
+# print(question,'\n',i7)
+
+
+# name=input('is:')
+# print(name.isalpha())#alpan stuguma arcok sax tara te che 
+
+# name=input('is:')
+# print(name.isdigit())#digit stuguma arcok sax tiva te che 
+
+# name=input('is:')
+# print(abs (5-6))#abs veradarcnuma bacasakan arjeke drakan 
+
+# name=input('is:')
+# print(name.upper())#poqric mecasarkum tarere
+
+# name=input('is:')
+# print(name.lower())#MECIC poqr
