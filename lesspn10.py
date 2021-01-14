0ae080a12b9f1d99497d1a97522ce78c2fa88661
Ashot-Sargsyan <77446410+Ashot-Sargsyan@users.noreply.github.com>
2021-01-14 18:18:33 +0400

    Loop

---
+++ lesspn10.py
@@ -0,0 +1,130 @@
+#loop koleso
+# names=['Artur','Jon','Ani']
+# for name in names:
+# 	print(name)
+
+
+
+# names='Artur'
+# for name in names:
+# 	print(name)
+
+
+# name='Ashot'
+# for i in name:
+# 	print(i)
+
+# 	if i=='s':
+# 		print(i)
+# 		break
+
+
+# name='Ashot'
+# for i in name:
+# 	if i=='s':
+# 		continue  #baca toxum interacyan aysiqn s u helnuma verev 
+# 	else:
+# 		print(i)
+
+
+# name='Ashot'
+# for i in name:
+# 	if i=='s':
+# 		pass  #nuin bana continue uxaki inke chi helnum verev chi sharnakum uxaki ban chi anum passe funkcyaneri hamara
+# 	else:
+# 		print(i)
+
+
+# for x in range(6):
+# 	print(x)
+
+# for Ashot in range(6):
+# 	print('Ashot')
+
+
+for x in range(2,30):
+	if x%2==0:
+		print(x)
+	
+
+
+
+# for x in range(1,10,2):
+# 	print(x)#erkusov gnuma 
+
+
+
+# n=int(input(''))
+# for x in range(0,n+1,2):
+# 	print(x)
+
+
+# color='red'
+# fruits='apple'
+# for x  in color:
+# 	for y in fruits:
+# 		print(x,y)
+
+
+
+# suit='xksxar'
+# collection='678910vdkt'
+# for x in suit:
+# 	for y in collection:
+# 		print(x,y)
+
+
+# i=1
+# while i<6:
+# 	print(i)
+# 	i+=1
+
+
+
+# i=1
+# while i<6:
+# 	print(i)
+# 	if i==4:
+# 		break
+# 	i+=1
+
+
+
+
+# while True:
+# 	number=input('number:')
+# 	if number.isdigit():
+# 		break
+# 	print('please input number')	
+
+
+
+# import datetime
+# start = datetime.datetime.now()
+# number1=int(input('numrer:'))
+# number2=int(input('numrer:'))
+# count=1
+# while True:
+# 	if count%number1==0 and count%number2==0:
+# 		print(count)
+# 		break
+# 	count+=1
+# end = datetime.datetime.now()
+# print(end - start)
+
+
+
+# import datetime
+# start = datetime.datetime.now()
+# number1=int(input('numrer:'))
+# number2=int(input('numrer:'))
+# res=number2*number1+1
+# if number1>number2:
+# 	res1=number1
+# else:
+# 	res1=number2
+# for i in range(res1,res):
+# 	print(i)
+# 	break
+# end = datetime.datetime.now()
+# print(end - start)
