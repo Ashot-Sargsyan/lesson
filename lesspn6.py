08601cea12d60d9466be93b6f4ca1f64224f2158
Ashot-Sargsyan <77446410+Ashot-Sargsyan@users.noreply.github.com>
2021-01-14 18:13:01 +0400

    Logical Operators

---
+++ lesspn6.py
@@ -0,0 +1,77 @@
+# returns false if both statements are false
+# x=4
+# result= x<5 and x>10 #and nshanakuma и
+# print(result)
+
+# x=4
+# result= x<5 or x>10 #or nshanakuma или
+# print(result)
+
+# x=4
+# result=not(x<5 or x>10) #not chiste darcnuma sxal
+# print(result)
+
+# x=4
+# print(id(x))
+
+# x='a'
+# y='a'
+# print(id(x))
+# print(id(y))
+# print(x is y)
+
+# x='a'
+# y='ab'
+# print(x is y)#is erp 2 popoxakane nuin hascein vraen talisa true ete che false
+
+# name='Ash'
+# print('A' in name)
+
+# name='Ash'
+# print('A' not in name)
+
+# name=input('what is your name? ')
+# # name=name.lower()#lower sax poqrasnuma 
+# name=name.upper()#upper sax mecasnuma
+# # name=name.title()#arachin tare mecasnuma bolor bareri
+# # name=name.capitalize()#arachin tare mecasnuma arachin bari
+# print('Ash'in name )
+
+
+
+# banana = input('Have you banana in fridge' ) == 'y'
+# print('I have banana',banana)
+# apple = input('Have you apple in fridge') == 'y'
+# print('I have apple',apple)
+# orange = input('Have you orange in fridge') == 'y'
+# print('I have orange',orange)
+# light= input('Have you light at home?' ) == 'y'
+# print(light)
+# res_banana = banana and not light
+# print('Get banana in fridge', res_banana)
+# res_apple = apple and not light
+# print('Get apple in fridge', res_apple)
+# res_orange = orange and not light
+# print('Get orange in fridge', res_orange)
+
+
+# frutis=input('have you at home frutis') == 'y'
+# print('i have at home frutis',frutis)
+# magazine=input('let is go to magazine buy frutis')=='y'
+# print('let is go to magazine buy frutis',magazine)
+# res=frutis and not magazine
+# print('let is go to magazine buy frutis',res)
+
+# frutis=input('have you at home frutis') == 'y'
+# print('i have at home frutis',frutis)
+# print('buy banana in magazine', not banana)
+
+
+
+# age=int(input('my age is :' ))
+# result_age=age>18 and age<25
+# status_weding=input('you married') == 'no'
+# rating_graduation=float(input('My rating:'))
+# result_rating_graduation=rating_graduation>=8 and rating_graduation<=10
+# res_work=result_age and status_weding and result_rating_graduation
+# print('yes you work our office',res_work)
