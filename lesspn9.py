43f8a8033e603181101a9e81d8017ef4752f97e6
Ashot-Sargsyan <77446410+Ashot-Sargsyan@users.noreply.github.com>
2021-01-14 18:17:37 +0400

    Python Exercises

---
+++ lesspn9.py
@@ -0,0 +1,71 @@
+'''1)'''
+
+# import random
+# number1=random.randint(1,100)
+# number2=int(input(''))
+# if 0<number2>100:
+# 	print('your number is dont betwin')
+
+# elif number1<number2:
+# 	print('number2 is big more number1',number1,number2)
+# elif number1>number2:
+# 	print('number1 is big more number2',number1,number2)
+# elif number1==number2:
+# 	print('number1 and number2 are equal',number1,number2)
+
+'''3)'''
+
+# Ashot=int(input('Ashot age:'))
+# Arnak=int(input('Arnak age:'))
+# Ani=int(input('Ani age:'))
+# if Ashot==Arnak or Arnak==Ani or Ashot==Ani:
+# 	print('your data entry does not meet the task condition')
+# 	exit()
+# elif Ashot>Arnak and Ashot>Ani:
+# 	print('Ashot is oldest')
+# elif Arnak>Ani and Arnak>Ashot:
+# 	print('Arnak is oldest')
+# elif Ani>Ashot and Ani>Arnak:
+# 	print('Ani is oldest')
+# elif Ashot<0 or Arnak<0 or Ani<0:
+# 	print('Error')
+
+'''4)'''
+# number=input('')
+# print(number[::-1],'number')
+
+
+'''5)'''
+# import random
+# while True:
+# 	game='rps'
+# 	siri=random.choice(game)
+# 	ashy = input('rps')
+# 	if siri == 'r' and ashy=='p' or siri=='p' and ashy=='s' or siri=='s' and ashy=='r' :
+# 		point=+1
+# 		print('Ashot the win')
+# 		break
+# 	elif not(siri == 'r' and ashy=='p' or siri=='p' and ashy=='s' or siri=='s' and ashy=='r' ):
+# 		point=+1
+# 		print('siri the win')
+# 		break
+
+
+'''6)'''
+# import random
+# siri=random.randint(1,100)
+# Ashot=int(input('Ashot Followers:'))
+# if Ashot<=siri  :
+# 	print('Siri is a bloger')
+
+# elif Ashot+Ashot*0.2<siri:
+# 	print('Siri is a bloger')
+# elif Ashot+Ashot*0.2>siri:
+# 	print('Ashot is a bloger')
+# else:
+# 	print('Error')
+
+
+'''7)'''
+virtual_machine=input('')
+human=input('')
