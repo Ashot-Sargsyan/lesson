a0cb325c3cbe898c9ed9fd8ce3c85851d47f10e2
Ashot-Sargsyan <77446410+Ashot-Sargsyan@users.noreply.github.com>
2021-01-14 18:12:20 +0400

    import Modules

---
+++ lesspn5.py
@@ -0,0 +1,138 @@
+# import math
+# print(math.pi)
+# print(math.sqrt(16))
+
+# from math import*#karanq grenq naev from math pi
+# print(pi)
+# print(sqrt(25))
+
+# import random
+# print(random.random())#0 minchev 1 random tver
+
+# import random
+# print(random.randint(0,20))# 0 minchev 20 random tver
+
+# import random
+# human=int(input('human number:'))
+# computer=random.randint(1,6)
+# res=human<computer
+# print(human,computer,res)
+
+# import random
+# x='abcdefgh'
+# print(random.choice(x))#patahakan miat tara hanum bari mech  
+
+
+# import random # random ddzevi parola tali vori mech ka 6 tar 2 hat tiv 
+# password=''
+# abc='ajfhjiaidsghfijahiffajisdgfvsd'
+# number='6561561561'
+# char=random.choice(abc)
+# password+=char
+# char=random.choice(abc)
+# password+=char
+# char=random.randint(0,9)
+# password+=str(char)#str tive bara darcnum
+# char=random.choice(abc)
+# password+=char
+# char=random.randint(0,9)
+# password+=str(char)
+# char=random.choice(abc)
+# password+=char
+# char=random.choice(abc)
+# password+=char
+# char=random.choice(abc)
+# password+=char
+# print(password)
+
+# import random as r #randome as-ov nshanakumem r
+# print(r.randrange(0,100,5))
+
+# import random as r
+# print(r.randrange(1,100,2))#kent tver random 1+2=3+2=5+7
+
+
+# import time
+# print('hi,my name is ash')#jamanaka tali arachin printic heto 5 varkyan heto erkrod printna berum
+# time.sleep(5)
+# print('hi')
+
+
+# import os
+# print(os.getcwd())#cuica talite vortexa gtnvum kokret faile
+
+
+# import calendar#kalendar
+# y=int(input('input the year:'))
+# m=int(input('input the month:'))
+# print(calendar.month(y,m))
+
+# import datetime
+# x = datetime.datetime.now()#datetime.now henc hima varkyanerov
+# print(x)
+# print(datetime.date.today())
+
+# import time # cragire sksuma ashxatel u verchum inchkana ashxatum
+# import datetime
+# start = datetime.datetime.now()#datetime.now henc hima varkyanerov
+
+# import random # random ddzevi parola tali vori mech ka 6 tar 2 hat tiv 
+# password=''
+# time.sleep(0.1)
+# abc='ajfhjiaidsghfijahiffajisdgfvsd'
+# number='6561561561'
+# char=random.choice(abc)
+# password+=char
+# char=random.choice(abc)
+# password+=char
+# char=random.randint(0,9)
+# password+=str(char)#str tive bara darcnum
+# char=random.choice(abc)
+# password+=char
+# char=random.randint(0,9)
+# password+=str(char)
+# char=random.choice(abc)
+# password+=char
+# char=random.choice(abc)
+# password+=char
+# char=random.choice(abc)
+# password+=char
+# end = datetime.datetime.now()#datetime.now henc hima varkyanerov
+# print(password,end - start)
+
+# import datetime
+# x=datetime.datetime(1996,4,21)  # konkret nshvac amis amsativ
+# print(x)
+
+# import datetime
+# x = datetime.datetime.now()
+# print(x.year)
+# print(x.strftime('%A'))#cuica tali konkret esorva ore
+
+# import datetime #asumem kani tarekanem asuma vor tvinem cnve
+# age=int(input('Jony is born:'))
+# x=datetime.datetime.now()
+# print(x.year-age)
+
+
+# import math
+# import random as r
+# x=r.randint(1,10)
+# s=2*math.pi*x**2
+# print(s,x)
+
+
+#6x**2+10x-1 gti x
+# import math
+# a=6
+# b=10
+# c=-1
+# D=b**2-(4*a*c)
+# print(D)
+# x1=(-b+math.sqrt(D))/(2*a)
+# x2=(-b-math.sqrt(D))/(2*a)
+# print(x1,x2)
+# while True:
+# 	print('sdsa')
+# 	if duq==3:
+# 		break
