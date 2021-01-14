5c79026f47c711d4529732fa218d8e104b489e14
Ashot-Sargsyan <77446410+Ashot-Sargsyan@users.noreply.github.com>
2021-01-14 18:09:50 +0400

    Mathematical Operators

---
+++ lesspn3.py
@@ -0,0 +1,141 @@
+# x=7
+# y=5
+# result=x%y
+# print(result)
+# print(x%y)
+
+# x=5
+# y=7
+# result=x%y
+# print(result)
+# print(x%y)
+
+# x=6
+# y=10
+# result=x%y
+# print(result)
+# print(x%y)
+
+
+# x=10
+# y=3
+# result=x//y
+# print(result)
+# print(x//y)
+
+# x=10
+# y=3
+# result=x%y
+# print(result)
+# print(x%y)
+
+
+# x=5
+# y=10
+# result=x//y
+# print(result)
+# print(x//y)
+
+
+# x=10
+# y=4
+# result=x//y
+# print(result)
+# print(x//y)
+
+
+# number=int(input('my number is od or even:' ))
+# result = number%2==0
+# print('your number is ' ,result)
+
+# balance=100
+# paychek=150
+# balance=balance+paychek
+# print(balance)
+
+# balance=100
+# balance=balance+150
+# balance+=150
+# print(balance)
+
+
+# balance=100
+# balance=balance+150
+# balance-=150
+# print(balance)
+
+
+
+# balance=100
+# balance=balance+150
+# balance*=150
+# print(balance)
+
+
+# balance=100
+# balance=balance+150
+# balance/=150
+# print(balance)
+
+
+# x=5
+# y=7
+# result=x==y
+# print(result)
+
+# x=5
+# y=7
+# result=x!=y
+# print(result)
+
+
+
+# card=0
+# card=card+50
+# card-=25
+# result=card==0
+# print(result)
+
+
+# balance = 0
+# paychek=int(input('$'))
+
+# balance += paychek
+# shop1=int(input('1 $ is : '))
+# balance-=shop1
+# result=balance>=0
+# print(result)
+# shop2=int(input('2 $ is : '))
+# balance-=shop2
+# result=balance>=0
+# print(result)
+
+
+# x=100
+# y=x*0.2
+# x+=y
+# print(x)
+
+
+
+# num1=int(input('number1: '))
+# num2=int(input('number2: '))
+# x=num2*0.2
+# num2+= x
+# print(num1>num2)
+
+# x='2'
+# print(x*3)
+
+
+# num1=int(input('num1 : '))
+# num1**=2
+# num2=int(input('num2 : '))
+# num2**=0.5
+# res = num1-num2 > 0
+# print(res)
+
+# nam1=int(input('nam1 : '))
+# nam1%=4
+# res = nam1 ==0
+# print(res)
