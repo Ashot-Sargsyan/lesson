2d64f70f7cc20658026503ae23164cdc03b069c4
Ashot-Sargsyan <77446410+Ashot-Sargsyan@users.noreply.github.com>
2021-01-14 18:29:38 +0400

    Set Frozenset

---
+++ lesspn19.py
@@ -0,0 +1,60 @@
+# set1={12,23,'11','hello',12}#krknvox arjeke kamsivole jnjuma set pahuma mi hat
+# print(set1)
+
+# this_set = {'apple','banana','cherry'}
+# for x in this_set:
+# 	print(x)
+
+# this_set={'apple','banana','cherry'}
+# print('banana' in this_set)
+
+# this_set={10,10,5}#gumarna hashvum bolor tveri
+# sum=0
+# for i in this_set:
+# 	sum+=i
+# print(sum)
+
+# print(set('hello'))#stexcuma nor set vekaluma amen mekic mi hat 
+
+
+'''apendner krknel listi,dicionary,tuple'''
+# x=set('bare')
+# x.add('v')#avelacnuma V tare
+# print(x)
+
+
+# x=set('bare')
+# x.add('v')#avelacnuma V tare
+# y=set('news')
+# x.update(y)#avelasnuma mi arjekic avel x.seti mech news
+# print(x)
+
+
+# x=set('bare')
+# x.remove(l)#remove jnjuma ete ka et tare kam tive ete che ka erora tali 
+# print(x)
+
+# x=set('bare')
+# x.discard(l)#discard jnjuma ete ka et tare kam tive ete che ka erora chi tali tali 
+# print(x)
+
+# set1={'a','b','c'}
+# set2={1,2,3}
+# set3=set1.union(set2)#unione taza seta sarkum u kcuma set3 mech set1 u set2 mechi exace
+# print(set3)
+
+
+# set1={12,23,11,'hello'}
+# set2={14,3,'141','word'}
+# print(set1.isdisjoint(set2))#ete che ka hamknox ban talisa true ete ka false
+
+
+# set1={12,2,14}
+# set2={10,5,4}
+# set3={12,50,6}
+# set1.update(set2)
+# print('','set1: ',set1,'\n','set1 and set2 equal:',not set1.isdisjoint(set3))
+
+
+
+'''parz tiv'''
