80e5d0224d91c2c59b3e64f24312647989980bee
Ashot-Sargsyan <77446410+Ashot-Sargsyan@users.noreply.github.com>
2021-01-14 18:08:20 +0400

    Variables

---
+++ lesspn2.py
@@ -0,0 +1,76 @@
+# x=5
+# print(type(x))# int- ineger( цулые числа)
+
+# y=7.4
+# print(type(y))# float (десятичные числа)
+
+# name = "Ash"
+# print(name,type(name))#str-string 
+
+# year='1996'
+# print(year,type(year)) #str-string
+
+# year = "1996"
+# print(year,type(year))
+# print(2020-int(year))
+# print(type((int(year))))
+
+# age = int(input('age is ?'))
+# print(type(age)) #kamel karaing greink age arac int kashxater
+
+# x= True
+# print(type(x)) #bool
+
+# y= False
+# print(type(y))#bool
+
+# x=7
+# y=8
+# result = x > y
+# print(type(result))
+
+
+# login = input('pleasy write your login: ')
+# pasword1=input('pleasy write your pasword: ')
+# pasword2=input('pleasy write your pasword reapit: ')
+# result=pasword1==pasword2
+# print('your pasword is ', result)
+
+
+# r=float(input('R is radius'))#ep grumenq float karanq int stanq baic int grelov chenq kara 5.1 tiv oktagorcenq aysiqn float
+# c= 2*(22/7)*r
+# print(c)
+
+# a=float(input('a: '))
+# b=float(input('b: '))
+# c = (a*a+b*b)**0.5
+# print(c)
+
+# a =float(input('a: '))
+# b = float(input('b: '))
+# c=(a+b)**2
+# print(c)
+
+
+# Armen =int(input('ko amsekan ashxatavatdze ?')) 
+# Arsen =int(input('ko amsekan ashxatavatdze ?'))
+# Gago =int(input('ko amsekan ashxatavatdze ?'))
+# print('',12*Armen,'\n',12*Arsen,'\n',12*Gago)
+# result=(12*Armen+12*Arsen+12*Gago)/3
+# print(result)
+
+
+# Armen =int(input('ko amsekan ashxatavatdze ?')) 
+# Arsen =int(input('ko amsekan ashxatavatdze ?'))
+# Gago =int(input('ko amsekan ashxatavatdze ?'))
+# salaryyearArmen = 12*Armen
+# salaryyearArsen = 12*Arsen
+# salaryyearGago = 12*Gago
+# result=(salaryyearArmen +salaryyearArsen+salaryyearGago)/3
+# print(result)
+
+# Dollar=float(input('$is: '))
+# Amd= Dollar*487
+# print(Amd)
+
+
