a72b66b2029eec868c1fc30443f9a150ffd02d07
Ashot-Sargsyan <77446410+Ashot-Sargsyan@users.noreply.github.com>
2021-01-14 18:13:58 +0400

    exercsises

---
+++ lesspn7.py
@@ -0,0 +1,80 @@
+# phone=input('Do you have a phone') == 'yes'
+# print(phone)
+# model=input('have you iphone 7') == 'yes'
+# print(model)
+# result=phone and model
+# print('Buy'result)
+
+# question1=input('Where are you from')=='Armeniya'
+# question2=input('Do you speak English')== 'yes'
+# question3=int(input('How old do you live in Armeniya'))==15
+# question4=input('Do you study')=='yes'
+# question5=input('Do you a work')== 'yes'
+# result=question1 and question2 and question3 and question4 or question5
+# print('Very well',result)
+
+# celsius=input('celsius and rain?').split()
+
+# if 'rain' in celsius:
+# 	print('rain take umbrela')
+# if int(celsius[0] )< 10:
+# 	print('take your jaket')
+# elif int(celsius[0] )> 10:
+# 	print('dont take your jaket')
+
+
+# import random
+# age=int(input('My age is :'))
+# result_age=age>=17 or age>=25
+# human=random.randint(17,25)
+# higher_education=input('do you have higher_education? ')=='yes'
+# work=input('do you experience?')=='yes'
+# human=result_age and work or higher_education
+
+# print('lets go','\n',age,'\n',result_age,'\n',human)
+# if human:
+# 	print('Very well')
+# else:
+# 	print('you didnt enter work our office')
+
+
+# cars ="BMW toyota hundai mercedes"
+# Faid=input('We have many different types of machines')
+# if Faid in cars:
+# 	print('Buy to cars')
+# else:
+# 	print('we do not sell you a car')
+
+
+# a=5
+# b=6
+# x=(a+b)**3
+# result_x=x
+# print(x)
+# z=a**3+3*a**2*b+3*a*b**2+b**3
+# print(z)
+
+# import random
+# computer=random.randint(1,10)
+# while True:
+# 	human=int(input('human number:'))
+# 	res=human==computer
+# 	if res:
+# 		print('my number is :',human ,'\n','number computer is :','\n',computer,'\n',res,)
+# 		break
+# 	else:
+# 		print('you number is FALSE EROR 5005',computer)
+
+
+
+# import random
+# siri=random.randint(1,10)
+# human=random.randint(1,10)
+# while True:
+# 	result=human==siri
+# 	if result:
+# 		print('my number is :',human ,'\n','number siri is :','\n',siri,'\n',result,)
+# 		break
+# 	else:
+# 		print('human number',human,'your number is FALSE EROR 5005',siri)
+	
