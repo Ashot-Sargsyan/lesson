1a73e0aa4159cc9dbca5d431a2856393382a68aa
Ashot-Sargsyan <77446410+Ashot-Sargsyan@users.noreply.github.com>
2021-01-14 18:26:19 +0400

    List Data Types

---
+++ lesspn14.py
@@ -0,0 +1,151 @@
+# fruits=['banana','apple','cherry']
+# print(fruits)
+
+
+# numbers=[34,56,-456,7.56,-2.34]#tpuma 56
+# print(numbers[-1])
+
+
+# fruits=['banana','apple','cherry','kiwi']
+# print(fruits[1:3])#tpuma apple,cherry
+# print(fruits[:3])#tpuma banana,apple,cherry
+# print(fruits[1:])#tpuma apple,cherry,kiwi
+# print(fruits[-3:-1])#tpuma apple,cherry
+
+
+# fruits=['banana','apple','cherry','kiwi']
+# fruits[1]='cherry'#appli texe gruma cherry
+# print(fruits)
+
+
+# fruits=['banana','apple','cherry']#fruits erkarucuna grum
+# print(len(fruits))
+
+
+fruits=['banana','apple','cherry']#verchum avelacnuma orange
+fruits.append('orange')
+print(fruits)
+
+# c=[]#c mech gruma orange
+# fruits=['banana','apple','cherry']#
+# c.append('orange')
+# print(c,fruits)
+
+
+# number_even=[]#gruma kent u zuig tvere
+# number_odd =[]
+# number=[1,2,3,4,5,6,7,8,9,10]
+# for i in number:
+# 	if i%2:
+# 		number_even.append(i)
+# 	else:
+# 		number_odd.append(i)
+# print('','Number odd:',number_odd,'\n','Number even:',number_even)		
+
+
+
+# fruits=['banana','apple','cherry']#gruma apple arach kcuma orange
+# fruits.insert(1,'orange')
+# print(fruits)
+
+
+
+# fruits=['banana','apple','cherry']#herasnuma konkret stringe aysiqn apple hunama toxuma banana u cherry
+# fruits.remove('apple')
+# print(fruits)
+
+
+
+# fruits=['banana','apple','cherry']
+# x=fruits.pop()#pope hanuma konkret en inch uzumenq ete chenq nshum () mech toverchica sksum hanel
+# print(fruits)
+
+
+
+# fruits=['banana','apple','cherry']
+# del fruits[2]#jnjuma konkret en vore nshaca
+# # del fruits#jnjuma amboxch fruitse 
+# print(fruits)
+
+
+# fruits=['banana','apple','cherry']
+# numbers=[34,56,-456,7.56,-2,34]
+# fruits.extend(numbers)#gumaruma
+# print(fruits)
+
+
+# numbers=[34,56,-456,7.56,-2,34]
+# numbers.sort()#sortavoruma poqric mec
+# print(numbers)
+
+
+# fruits=['banana','apple','cherry']
+# fruits.sort()#arachin tarova sortavorumest aibubeni
+# print(fruits)
+
+# numbers=[34,56,-456,7.56,-2.31]
+# numbers.reverse()#fracnuma hetevic racha grum
+# print(numbers)
+
+
+# fruits=['banana','apple','cherry']
+# fruits.reverse()#fracnuma hetevic arach
+# print(fruits)
+
+
+# numbers=[34,56,-456,7.56,-2.31]
+# print(max(numbers),min(numbers))
+
+
+
+# my_list=[12,2,4,65,23,-23]
+# first=my_list[0]
+# for i in my_list:
+# 	if i>first:
+# 		first=i
+# print(first)
+
+
+# my_list=[12,2,4,65,23,-23]
+# first=my_list[0]
+# for i in my_list:
+# 	if i<first:
+# 		first=i
+# print(first)
+# print(sum(my_list))
+
+
+# my_list=[12,2,4,65,23,-23]
+# x=0
+# for i in my_list:
+# 	x+=i
+# print(x)
+
+
+# my_list=[12,2,4,65,23,-23]
+# x=1
+# for i in my_list:
+# 	x*=i
+# print(x)
+
+
+# my_list=[12,2,4,65,23,-23]#65 texna cuic tali
+# x=65
+# c=0
+# for i in my_list:
+# 	if i==x:
+# 		break
+# 	else:
+# 		c+=1
+# print(c)
+
+
+
+# my_list=[12,2,4,65,23,-23]
+# x=65
+# c=0
+# for i in my_list:
+# 	if i==x:
+# 		c+=1#kani hat unem 65
+		
+# print(c)
